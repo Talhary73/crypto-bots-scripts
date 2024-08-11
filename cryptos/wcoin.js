a685eb7557fbf280d3ed328b69691e53d1a30365
Muhammad Talha <talhariaz5425869@gmail.com>
2024-08-11 20:35:48 +0500

    Update wcoin.js

--- cryptos/wcoin.js
+++ cryptos/wcoin.js
@@ -8,7 +8,26 @@ const urls = [
 let browser = null
 const {ActivatedPuppeteer} = require('./puppeteer')
 const launchBrowser = async()=>{
-  browser = await ActivatedPuppeteer()
+  browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      ...chromium.args,
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu',
+      '--window-size=360,640',
+    ],
+    defaultViewport: {
+      width: 360,
+      height: 640,
+      isMobile: true,
+      hasTouch: true,
+    },
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+  });
 }
 
 const userAgent = 'Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36';
